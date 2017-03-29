2f58260ee932277a96932f6ed613cacd5ab04967
Administrator <Administrator@SLECZHUSHIMU-PC>
2017-03-29 17:07:57 +0800

    添加项目文件。

---
+++ MokeWebServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MokeWebServer.MvcApplication" Language="C#" %>
---
+++ MokeWebServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MokeWebServer.Startup))]
+namespace MokeWebServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
